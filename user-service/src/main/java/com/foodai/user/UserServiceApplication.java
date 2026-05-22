e3f80ec1f166183d128a2e557f9564fa253c65de
Varun Buddha <varunbuddha2002@gmail.com>
2026-05-22 15:28:46 +0530

    initialize FoodAI microservices with Maven POM files and basic Spring Boot applications

--- user-service/src/main/java/com/foodai/user/UserServiceApplication.java
+++ user-service/src/main/java/com/foodai/user/UserServiceApplication.java
@@ -3,7 +3,7 @@ package com.foodai.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.foodai")
 public class UserServiceApplication {
 
     public static void main(String[] args) {
